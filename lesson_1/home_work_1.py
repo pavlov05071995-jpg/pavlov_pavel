852001e30b40cdb43922ae90a9651c36dfc77287
Павел Павлов <you@example.com>
2025-09-17 08:10:43 +0300

    home work 1

---
+++ lesson_1/home_work_1.py
@@ -0,0 +1,32 @@
+# a=7
+# b=8.9
+# str_1="легко в ИТ"
+# b=a
+# a=8
+# print(b)
+# a=b+c=0
+# a, b,c=0,0,0
+# a,b=8,10
+# a,b=b,a
+# print(a)
+# print(b)
+# print(type(a))
+# print(type(str_1))
+# print(type(b))
+from itertools import count
+
+# message="Hello World"
+# count=0
+# Count=0
+#
+# True=5
+# count=3
+# and=12
+
+a=c=4
+b,d="какая то строка", "вторая с трока"
+
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
